933747c2c0def9eb8adf55059cec6bdbdedbe933
Hkumar-me <141249568+Hkumar-me@users.noreply.github.com>
2024-02-17 23:44:58 +0530

    Update UI and fix bugs

--- src/components/Contents/Contents.jsx
+++ src/components/Contents/Contents.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Contents.css";
 import { Link } from "react-router-dom";
 
 function Contents() {
+
+  const scrollSectionRef = useRef(null);
+  const scrollSectionRef2 = useRef(null);
+
+  const [translation, setTranslation] = useState(0);
+
+  const handleScrollRight = () => {
+    const scrollSection = scrollSectionRef2.current;
+    const sectionWidth = scrollSection.offsetWidth;
+    setTranslation((prevTranslation) => prevTranslation - sectionWidth);
+  };
   let sections = [
     {
       section_name: "Recommended for you",
@@ -27,6 +38,47 @@ function Contents() {
           rating: 9,
           poster: "intersteller.jpg",
         },
+        {
+          name: "Godzilla",
+          rating: 8,
+          poster: "godzilla.jpg",
+        },
+        {
+          name: "Batman",
+          rating: 7,
+          poster: "batman.jpg",
+        },
+        {
+          name: "Godzilla",
+          rating: 8,
+          poster: "godzilla.jpg",
+        },
+        {
+          name: "Batman",
+          rating: 7,
+          poster: "batman.jpg",
+        },
+        {
+          name: "Enola Holmes",
+          rating: 8,
+          poster: "enola_holmes.jpg",
+        },
+        {
+          name: "Intersteller",
+          rating: 9,
+          poster: "intersteller.jpg",
+        },
+        {
+          name: "Godzilla",
+          rating: 8,
+          poster: "godzilla.jpg",
+        },
+        {
+          name: "Batman",
+          rating: 7,
+          poster: "batman.jpg",
+        },
+       
       ],
     },
     {
@@ -51,6 +103,45 @@ function Contents() {
           name: "Intersteller",
           rating: 9,
           poster: "intersteller.jpg",
+        },
+        {
+          name: "Godzilla",
+          rating: 8,
+          poster: "godzilla.jpg",
+        },
+        {
+          name: "Batman",
+          rating: 7,
+          poster: "batman.jpg",
+        }, {
+          name: "Godzilla",
+          rating: 8,
+          poster: "godzilla.jpg",
+        },
+        {
+          name: "Batman",
+          rating: 7,
+          poster: "batman.jpg",
+        },
+        {
+          name: "Enola Holmes",
+          rating: 8,
+          poster: "enola_holmes.jpg",
+        },
+        {
+          name: "Intersteller",
+          rating: 9,
+          poster: "intersteller.jpg",
+        },
+        {
+          name: "Godzilla",
+          rating: 8,
+          poster: "godzilla.jpg",
+        },
+        {
+          name: "Batman",
+          rating: 7,
+          poster: "batman.jpg",
         },
       ],
     },
@@ -77,6 +168,16 @@ function Contents() {
           rating: 9,
           poster: "intersteller.jpg",
         },
+        {
+          name: "Godzilla",
+          rating: 8,
+          poster: "godzilla.jpg",
+        },
+        {
+          name: "Batman",
+          rating: 7,
+          poster: "batman.jpg",
+        },
       ],
     },
   ];
@@ -88,12 +189,11 @@ function Contents() {
           <div>
             
             <div className="section">
-           
               <h2>{element.section_name}</h2>
-              <div className="cards">
+              <div className="cards" >
                 {element.list.map((element) => {
                   return ( <Link to={`/deflix.react/home/movies/${element.name}`} onClick={scrollToTop}>
-                    <div className="card">
+                    <div className="card" ref={scrollSectionRef}>
                       <img src={`images/${element.poster}`} alt="" />
                       <div className="info">
                         <h3>{element.name}</h3>
@@ -130,17 +230,21 @@ function Contents() {
               {element.list.map((element) => {
                 return (
                 <Link to={`/deflix.react/home/movies/${element.name}` } onClick={scrollToTop}>
-                  <div className="card">
-                    <img src={`images/${element.poster}`} alt="" />
+                  <div className="card" ref={scrollSectionRef2} style={{ transform: `translateX(${translation}px)` }}>
+                    <img src={`images/${element.poster}` } alt="" />
                     <div className="info">
                       <h3>{element.name}</h3>
                       <h4>{element.rating} / 10</h4>
                     </div>
                   </div>
                   </Link>
+
                 );
               })}
+              
             </div>
+            
+            <button onClick={handleScrollRight}>Scroll Right</button>
           </div>
         );
       })}
